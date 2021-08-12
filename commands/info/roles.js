1c7adbc2490a4ec52cc3b99024b57abb58818f3d
FnrDev <ah2014bh@outlook.com>
2021-08-12 20:13:33 +0300

    roles command

---
+++ commands/info/roles.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "roles",
+    description: "Get a list of roles in the server",
+    timeout: 3000,
+    run: async(interaction, client) => {
+        interaction.reply({ content: `\`\`\`\n${interaction.guild.roles.cache.sort((a,b) => b.position - a.position).map(r => r.name).join("\n")}\`\`\`` })
+    }
+}
