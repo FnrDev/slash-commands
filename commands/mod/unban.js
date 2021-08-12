acb13452d738f7e934a9681d557b5368c57dd0f3
FnrDev <ah2014bh@outlook.com>
2021-08-12 23:53:28 +0300

    unban command

---
+++ commands/mod/unban.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "unban",
+    description: "Unban user from this server",
+    options: [
+        {
+            name: "user",
+            description: "user to unban",
+            type: 3,
+            required: true
+        }
+    ],
+    timeout: 3000,
+    run: async(interaction, client) => {
+        if (!interaction.member.permissions.has('BAN_MEMBERS')) {
+            return interaction.reply({ content: "You dont have permission to do this command!", ephemeral: true })
+        }
+        const user = interaction.options.getString('user');
+        try {
+            await interaction.guild.bans.remove(user);
+            const unbanUser = await client.users.fetch(user)
+            interaction.reply({ content: `✅ **@${unbanUser.username} has been unbanned**` })
+        } catch (e) {
+            console.error(e)
+            return interaction.reply({ content: `Error: ${e}` })
+        } 
+    }
+}
