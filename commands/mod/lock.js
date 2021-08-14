8e1dd9c7ca215764257d47232a6f8dc45481b514
FnrDev <ah2014bh@outlook.com>
2021-08-14 19:08:34 +0300

    Added reason in audit log

--- commands/mod/lock.js
+++ commands/mod/lock.js
@@ -1,5 +1,3 @@
-const Discord = require('discord.js');
-
 module.exports = {
     name: "lock",
     description: "🔒 Disables @everyone from sending messages in specific channel",
@@ -11,12 +9,12 @@ module.exports = {
         }
     ],
     timeout: 3000,
-    run: async(interaction, client) => {
+    run: async(interaction) => {
         if (!interaction.member.permissions.has('MANAGE_CHANNELS')) {
             return interaction.reply({ content: "You dont have permission to do this command!", ephemeral: true })
         }
         const channel = interaction.options.getChannel('channel') || interaction.channel;
-        channel.permissionOverwrites.edit(interaction.guild.id, { SEND_MESSAGES: false });
+        await channel.permissionOverwrites.edit(interaction.guild.id, { SEND_MESSAGES: false });
         interaction.reply({ content: `**🔒 ${channel} has been locked.**` })
     }
 }
--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -17,32 +17,32 @@ module.exports = {
         }
     ],
     timeout: 3000,
-    run: async(interaction, client) => {
+    run: async(interaction) => {
         if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
             return interaction.reply({ content: ":x: You dont have permission to do this command!", ephemeral: true })
         }
         const user = interaction.options.getMember('user');
         const reason = interaction.options.getString('reason') || '';
-        if (interaction.user.id == user.id) {
+        if (interaction.user.id === user.id) {
             return interaction.reply({ content: ":x: You can't mute yourself" })
         }
         const botRole = interaction.guild.me.roles.highest.position;
         const role = user.roles.highest.position;
         const authorRole = interaction.member.roles.highest.position;
         if (authorRole <= role) {
-            const embed = new MessageEmbed()
+            const embed = new Discord.MessageEmbed()
             .setTitle("I can't mute this member because that member has role position is higher than my role or same as you!")
             .setColor('#ff0000')
             return interaction.reply({ embeds: [embed] })
         }
         if (botRole <= role) {
-            const embed = new MessageEmbed()
+            const embed = new Discord.MessageEmbed()
             .setTitle("I can't mute this member because that member has role position is higher than my role or same as you!")
             .setColor('#ff0000')
             return interaction.reply({ embeds: [embed] })
         }
         try {
-            const muteRole = interaction.guild.roles.cache.find(role => role.name == 'Muted');
+            const muteRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
             if (!muteRole) {
                 this.createMutedRole = await interaction.guild.roles.create({
                     name: "Muted",
@@ -55,7 +55,7 @@ module.exports = {
             interaction.guild.channels.cache.forEach((channel) => {
                 channel.permissionOverwrites.edit(this.createMutedRole.id, { SEND_MESSAGES: false, ADD_REACTIONS: false })
             })
-            user.roles.add(this.createMutedRole, `By: ${interaction.user.tag}`)
+            await user.roles.add(this.createMutedRole, `By: ${interaction.user.tag} Reason: ${reason}`)
             interaction.reply({ content: `✅ **@${user.user.username} has been muted!**` })
         } catch (e) {
             console.error(e);
