c50c9ee87b36f50a1d24b86782427730f28f4f2a
FnrDev <ah2014bh@outlook.com>
2021-08-14 14:32:13 +0300

    avatar command

---
+++ commands/info/avatar.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "avatar",
+    description: "Get user avatar",
+    options: [
+        {
+            name: "user",
+            description: "User to get avatar",
+            type: 6,
+        }
+    ],
+    run: async(interaction, client) => {
+        const user = interaction.options.getUser('user') || interaction.user;
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(user.tag, user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`[Avatar Link](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`)
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+        .setFooter(`Requested By ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+        interaction.reply({ embeds: [embed] })
+    }
+}
