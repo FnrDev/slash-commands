5af7450f86e9e1eda7f8af0e252e1fc00a5925f7
FnrDev <ah2014bh@outlook.com>
2021-08-18 01:00:19 +0300

    sub command for role

--- commands/mod/role.js
+++ commands/mod/role.js
@@ -1,53 +1,110 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: "role",
     description: "role someone.",
-    timeout: 3000,
     options: [
         {
-            name: "user",
-            type: 6, // type has been changed check <https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type>
-            description: "user to give a role",
-            required: true
+            name: "bots",
+            description: "Gives a role to all bots in this server.",
+            type: 1,
+            options: [
+                {
+                    name: "type",
+                    description: "pick a type",
+                    type: 3,
+                    required: true,
+                    choices: [
+                        {
+                            name: "Give",
+                            value: "give"
+                        },
+                        {
+                            name: "Remove",
+                            value: "remove"
+                        }
+                    ]
+                },
+                {
+                    name: "role",
+                    description: "Role to give all bots.",
+                    type: 8,
+                    required: true
+                }
+            ]
         },
         {
-            name: "role",
-            type: 8, // type has been changed check <https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type>
-            description: "role to give to user",
-            required: true
+            name: "user",
+            description: "Gives a user role.",
+            type: 1,
+            options: [
+                {
+                    name: "user",
+                    description: "user to give a role.",
+                    type: 6,
+                    required: true
+                },
+                {
+                    name: "role",
+                    description: "Role to give a user.",
+                    type: 8,
+                    required: true
+                }
+            ],
         }
     ],
-    run: async(interaction, client) => {
-        const user = interaction.options.getMember('user'); // access to guild member obj <https://discord.js.org/#/docs/main/stable/class/GuildMember>
-        const role = interaction.options.getRole('role'); // access to role obj <https://discord.js.org/#/docs/main/stable/class/Role>
+    timeout: 3000,
+    run: async(interaction) => {
         if (!interaction.member.permissions.has('MANAGE_ROLES')) {
             return interaction.reply({ content: "You dont have permission to do this command", ephemeral: true })
         }
-        const botRole = interaction.guild.me.roles.highest.position;
-        const roleToGet = user.roles.highest.position;
-        const authorRole = interaction.member.roles.highest.position;
-        if (authorRole <= roleToGet) {
-            const embed = new MessageEmbed()
-            .setTitle("I can't role this member because that member has role position is higher than my role or same as you!")
-            .setColor('#ff0000')
-            return interaction.reply({ embeds: [embed] })
+        const user = interaction.options.getMember('user'); // access to guild member obj <https://discord.js.org/#/docs/main/stable/class/GuildMember>
+        const role = interaction.options.getRole('role'); // access to role obj <https://discord.js.org/#/docs/main/stable/class/Role>
+        // User Sub Command
+        if (interaction.options.getSubcommand() === 'user') {
+            const botRole = interaction.guild.me.roles.highest.position;
+            const roleToGet = user.roles.highest.position;
+            const authorRole = interaction.member.roles.highest.position;
+            if (authorRole <= roleToGet) {
+                const embed = new MessageEmbed()
+                .setTitle("I can't role this member because that member has role position is higher than my role or same as you!")
+                .setColor('#ff0000')
+                return interaction.reply({ embeds: [embed] })
+            }
+            if (botRole <= roleToGet) {
+                const embed = new MessageEmbed()
+                .setTitle("I can't role this member because that member has role position is higher than my role or same as you!")
+                .setColor('#ff0000')
+                return interaction.reply({ embeds: [embed] })
+            }
+            let addRoles;
+            if (user._roles.includes(role.id)) {
+                addRoles = '-'
+                user.roles.remove(role, `By: ${interaction.user.tag}`)
+                interaction.reply({ content: `✅ Changed role for ${user}, **${addRoles}${role.name}**` })
+            } else {
+                addRoles = '+'
+                user.roles.add(role, `By: ${interaction.user.tag}`)
+                interaction.reply({ content: `✅ Changed role for ${user}, **${addRoles}${role.name}**` })
+            }
         }
-        if (botRole <= roleToGet) {
-            const embed = new MessageEmbed()
-            .setTitle("I can't role this member because that member has role position is higher than my role or same as you!")
-            .setColor('#ff0000')
-            return interaction.reply({ embeds: [embed] })
-        }
-        let addRoles;
-        if (user._roles.includes(role.id)) {
-            addRoles = '-'
-            user.roles.remove(role, `By: ${interaction.user.tag}`)
-            interaction.reply({ content: `✅ Changed role for ${user}, **${addRoles}${role.name}**` })
-        } else {
-            addRoles = '+'
-            user.roles.add(role, `By: ${interaction.user.tag}`)
-            interaction.reply({ content: `✅ Changed role for ${user}, **${addRoles}${role.name}**` })
+        // Bots Sub Command
+        if (interaction.options.getSubcommand() === 'bots') {
+            if (interaction.options._hoistedOptions.find(r => r.value === 'give')) {
+                interaction.guild.members.cache.filter(r => r.user.bot).forEach(bot => {
+                    bot.roles.add(role, `By: ${interaction.user.tag}`)
+                })
+                const totalBots = interaction.guild.members.cache.filter(r => r.user.bot).size
+                console.log(totalBots)
+                return interaction.reply({ content: `✅ Changed role for **${totalBots}** bots, **+${role.name}**` })
+            }
+            if (interaction.options._hoistedOptions.find(r => r.value == 'remove')) {
+                interaction.guild.members.cache.filter(r => r.user.bot).forEach(bot => {
+                    bot.roles.remove(role, `By: ${interaction.user.tag}`)
+                })
+                const totalBots = interaction.guild.members.cache.filter(r => r.user.bot).size
+                return interaction.reply({ content: `✅ Changed role for **${totalBots}** bots, **-${role.name}**` })
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const fs = require("fs");
 const config = require('./config.json');
 const token = config.token;
@@ -38,5 +38,6 @@ const rest = new REST({ version: "9" }).setToken(token);
 ["handlers", "events"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
+
   
 client.login(token);
