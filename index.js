acd44e889b5823cc6f31c58cb95d6dc105a510ee
FnrDev <ah2014bh@outlook.com>
2021-08-27 04:40:10 +0300

    invite intents

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INVITES] });
 const fs = require("fs");
 const config = require('./config.json');
 const token = config.token;
