95a797097767be9b18b713d07a03212df16f16d2
FnrDev <ah2014bh@outlook.com>
2021-08-31 20:10:33 +0300

    guild unban event

---
+++ events/client/guildBanRemove.js
@@ -0,0 +1,22 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports = async(client, ban) => {
+    const logChannel = await client.channels.cache.get(config.log_channel_id);
+    const allLogs = await ban.guild.fetchAuditLogs({ type: "MEMBER_BAN_REMOVE" });
+    const fetchModerator = allLogs.entries.first();
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(ban.user.tag, ban.user.displayAvatarURL({ dynamic: true }))
+    .setDescription(`**🔨 <@${ban.user.id}> unbanned**`)
+    .setThumbnail(ban.user.displayAvatarURL({ dynamic: true }))
+    .setTimestamp()
+    .setFooter(ban.guild.name, ban.guild.iconURL({ dynamic: true }))
+    .addFields(
+        {
+            name: "Responsible Moderator:",
+            value: `<@${fetchModerator.executor.id}>`,
+            inline: true
+        },
+    )
+    logChannel.send({ embeds: [embed] })
+}
