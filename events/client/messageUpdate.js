a7ff6149d81b9eec4254b3a3bd34ffd826af21c4
FnrDev <ah2014bh@outlook.com>
2021-08-31 22:04:29 +0300

    message update event

---
+++ events/client/messageUpdate.js
@@ -0,0 +1,23 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports = async(client, oldMessgae, newMessage) => {
+    const logChannel = client.channels.cache.get(config.log_channel_id);
+    if (!logChannel) return;
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL({ dynamic: true }))
+    .setTimestamp()
+    .setFooter(newMessage.author.tag, newMessage.author.displayAvatarURL({ dynamic: true }))
+    .setDescription(`📝 **Message sent by ${newMessage.author} edited in ${newMessage.channel}.** [Jump To Message](https://discord.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
+    .addFields(
+        {
+            name: "Old:",
+            value: `\`\`\`\n${oldMessgae.content}\`\`\``
+        },
+        {
+            name: "New:",
+            value: `\`\`\`\n${newMessage.content}\`\`\``
+        }
+    )
+    return logChannel.send({ embeds: [embed] })
+}
