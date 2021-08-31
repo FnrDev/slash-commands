4d9bbbd10b0cd1cfd71067b6d70cce0b50138a7f
FnrDev <ah2014bh@outlook.com>
2021-08-31 21:35:00 +0300

    invite delete event

---
+++ events/client/inviteDelete.js
@@ -0,0 +1,34 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+const humanizeDuration = require("humanize-duration");
+
+module.exports = async(client, invite) => {
+    const logChannel = await client.channels.cache.get(config.log_channel_id);
+    if (!logChannel) return;
+    const allLogs = await invite.guild.fetchAuditLogs({ type: "INVITE_CREATE" });
+    const fetchModerator = allLogs.entries.first();
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(invite.guild.name, invite.guild.iconURL({ dynamic: true }))
+    .setTitle('👩‍👧‍👦 Invite Deleted')
+    .setDescription(`**<@${fetchModerator.executor.id}> has deleted a invite for ${invite.channel} channel**`)
+    .setTimestamp()
+    .setFooter(fetchModerator.executor.tag, fetchModerator.executor.displayAvatarURL({ dynamic: true }))
+    .addFields(
+        {
+            name: "Invite link:",
+            value: `[Invite](https://discord.gg/${invite.code})`,
+            inline: true
+        },
+        {
+            name: "Invite Code:",
+            value: invite.code,
+            inline: true
+        },
+        {
+            name: "Responsible Moderator:",
+            value: `<@${fetchModerator.executor.id}>`,
+            inline: true
+        },
+    )
+    return logChannel.send({ embeds: [embed] })
+}
