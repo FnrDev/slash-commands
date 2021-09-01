22695cbca1387040b4187f0ce109bacc3e4b5336
FnrDev <ah2014bh@outlook.com>
2021-09-01 23:42:51 +0300

    delete emoji event

---
+++ events/client/emojiDelete.js
@@ -0,0 +1,22 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports = async(client, emoji) => {
+    const logChannel = client.channels.cache.get(config.log_channel_id);
+    if (!logChannel) return;
+    const fetchEmojiAuthor = await emoji.fetchAuthor();
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(emoji.guild.name, emoji.guild.iconURL({ dynamic: true }))
+    .setTitle('🥳 Emoji Deleted')
+    .setDescription(`**${fetchEmojiAuthor} has deleted <:${emoji.name}:${emoji.id}> emoji!**`)
+    .setThumbnail(emoji.url)
+    .setFooter(fetchEmojiAuthor.tag, fetchEmojiAuthor.displayAvatarURL({ dynamic: true }))
+    .setTimestamp()
+    .addFields(
+        {
+            name: "Responsible Moderator:",
+            value: `<@${fetchEmojiAuthor.id}>`
+        },
+    )
+    return logChannel.send({ embeds: [embed] })
+}
