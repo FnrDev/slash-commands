6b532437a53413e4b45a49c7feb1197e6683cf22
FnrDev <ah2014bh@outlook.com>
2021-10-13 16:34:28 +0300

    improvements

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ const rest = new REST({ version: "9" }).setToken(token);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands.'.yellow);
+		console.log('[Discord API] Started refreshing application (/) commands.'.yellow);
 		await rest.put(
 			// if you want to make your slash commands in all guilds use "applicationCommands("CLIENT_ID")"
 			Routes.applicationGuildCommands(config.botID, config.serverID),
 			{ body: commands },
 		);
-		console.log('Successfully reloaded application (/) commands.'.green);
+		console.log('[Discord API] Successfully reloaded application (/) commands.'.green);
 	} catch (error) {
 		console.error(error);
 	}
