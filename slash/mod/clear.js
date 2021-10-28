464328e8feb290234475d67332bdc29050ccf872
FnrDev <ah2014bh@outlook.com>
2021-10-28 12:29:45 +0300

    filter by role

--- slash/mod/clear.js
+++ slash/mod/clear.js
@@ -15,12 +15,18 @@ module.exports = {
             name: "user",
             description: "User to clear messsages for",
             type: 6
+        },
+        {
+            name: "role",
+            description: "Clear messages from role",
+            type: 8
         }
     ],
     timeout: 5000,
     run: async(interaction, client) => {
         let deleteAmount = interaction.options.getNumber('number_of_messages');
         const user = interaction.options.getUser('user');
+        const role = interaction.options.getRole('role');
         if (deleteAmount > 100) {
             deleteAmount = 100
         }
@@ -28,6 +34,10 @@ module.exports = {
         if (user) {
             fetchedMessage.filter(r => r.author.id === user.id).forEach(msg => msg.delete());
             return interaction.reply({ content: `✅ Successfully deleted ${fetchedMessage.size} messages` });
+        }
+        if (role) {
+            fetchedMessage.filter(r => r.member.roles.cache.has(role.id)).forEach(msg => msg.delete());
+            return interaction.reply({ content: `✅ Successfully deleted ${fetchedMessage.size} messages` })
         }
         fetchedMessage.forEach(msg => msg.delete())
         interaction.reply({ content: `✅ Successfully deleted ${fetchedMessage.size} messages` });
