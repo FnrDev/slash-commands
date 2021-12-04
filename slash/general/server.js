e9fac505aacb01cac13fcaf5520387f6d223a74b
FnrDev <ah2014bh@outlook.com>
2021-12-04 21:20:24 +0300

    /server improvements

--- slash/general/server.js
+++ slash/general/server.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.js');
-const humanizeDuration = require("humanize-duration");
 
 module.exports = {
     name: "server",
     description: "Get info about server",
     timeout: 3000,
     run: async(interaction) => {
-        const createdServer = Date.now() - interaction.guild.createdTimestamp;
         const embed = new Discord.MessageEmbed()
         .setAuthor(interaction.guild.name, interaction.guild.iconURL({ dynamic: true }))
         .setColor('RANDOM')
@@ -19,7 +17,7 @@ module.exports = {
             },
             {
                 name: ":calendar: Created On:",
-                value: `${interaction.guild.createdAt.toLocaleString()}\n${humanizeDuration(createdServer, { largest: 1 })} ago`,
+                value: `<t:${Math.floor(interaction.guild.createdTimestamp / 1000)}:f> | <t:${Math.floor(interaction.guild.createdTimestamp / 1000)}:R>`,
                 inline: true
             },
             {
@@ -43,102 +41,8 @@ module.exports = {
                 inline: true
             },
         )
-        const row = new Discord.MessageActionRow()
-        .addComponents(
-            new Discord.MessageButton()
-            .setCustomId('roles')
-            .setStyle('PRIMARY')
-            .setEmoji('🔒')
-            .setLabel('Sever Roles')
-        )
-        .addComponents(
-            new Discord.MessageButton()
-            .setCustomId('channels')
-            .setEmoji('💬')
-            .setStyle('SUCCESS')
-            .setLabel('Text Channels')
-        )
-        .addComponents(
-            new Discord.MessageButton()
-            .setCustomId('voice')
-            .setEmoji('🔊')
-            .setStyle('SECONDARY')
-            .setLabel('Voice Channels')
-        )
-        .addComponents(
-            new Discord.MessageButton()
-            .setCustomId('features')
-            .setEmoji('🎉')
-            .setStyle('SUCCESS')
-            .setLabel('Server Features')
-        )
-        interaction.reply({ embeds: [embed], components: [row] })
-        try {
-            const filter = i => i.customId === 'roles' || 'channels' || 'voice' || 'features' && i.user.id === interaction.user.id;
-            const collector = interaction.channel.createMessageComponentCollector({ filter: filter, max: 4 });
-            collector.on('collect', async i => {
-                if (i.customId === 'roles') {
-                    await i.deferReply();
-                    const editRow = row.addComponents(
-                        new Discord.MessageButton()
-                        .setCustomId('ended')
-                        .setStyle('DANGER')
-                        .setLabel('Server Roles')
-                        .setDisabled(true)
-                    )
-                    row.spliceComponents(0, 1)
-                    let rolesLoop = '';
-                    interaction.guild.roles.cache.sort((a, b) => b.position - a.position).forEach(role => {
-                        rolesLoop += `${role.name}         ${role.members.size} members\n`
-                    })
-                    interaction.editReply({ components: [editRow] })
-                    return await i.editReply({ content: `**${interaction.guild.name} Roles:**\`\`\`\n${rolesLoop}\`\`\``, embeds: [], components: [] })
-                }
-                if (i.customId === 'channels') {
-                    await i.deferReply();
-                    const editRow = row.addComponents(
-                        new Discord.MessageButton()
-                        .setCustomId('ended_channel')
-                        .setStyle('DANGER')
-                        .setLabel('Text Channels')
-                        .setDisabled(true)
-                    )
-                    row.spliceComponents(0, 1)
-                    interaction.editReply({ components: [editRow] })
-                    const mapChannels = interaction.guild.channels.cache.sort((a, b) => a.position - b.position).filter(r => r.type === 'GUILD_TEXT').map(r => r.name).join("\n");
-                    return await i.editReply({ content: `**${interaction.guild.name} Text Channels:**\`\`\`\n${mapChannels}\`\`\``, embeds: [], components: [] });
-                }
-                if (i.customId === 'voice') {
-                    await i.deferReply();
-                    const editRow = row.addComponents(
-                        new Discord.MessageButton()
-                        .setCustomId('ended_voice')
-                        .setStyle('DANGER')
-                        .setLabel('Voice Channels')
-                        .setDisabled(true)
-                    )
-                    row.spliceComponents(0, 1)
-                    interaction.editReply({ components: [editRow] })
-                    const mapVoiceChannels = interaction.guild.channels.cache.sort((a, b) => a.position - b.position).filter(r => r.type === 'GUILD_VOICE').map(r => r.name).join("\n");
-                    return await i.editReply({ content: `**${interaction.guild.name} Voice Channels**\`\`\`\n${mapVoiceChannels}\`\`\``, embeds: [], components: []  })
-                }
-                if (i.customId === 'features') {
-                    await i.deferReply();
-                    const editRow = row.addComponents(
-                        new Discord.MessageButton()
-                        .setCustomId('ended_features')
-                        .setStyle('DANGER')
-                        .setLabel('Server Features')
-                        .setDisabled(true)
-                    )
-                    row.spliceComponents(0, 1)
-                    interaction.editReply({ components: [editRow] })
-                    const features = interaction.guild.features.join("\n");
-                    return await i.editReply({ content: `**${interaction.guild.name} Features:**\n\`\`\`${features}\`\`\`` })
-                }
-            })
-        } catch (e) {
-            console.error(e)
-        }
+        interaction.reply({
+            embeds: [embed]
+        })
     }
 }
