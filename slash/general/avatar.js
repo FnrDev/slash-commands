e9b38fab37d0d0d38732a45b5b0a5fd2406919ca
Fnr <ah2014bh@outlook.com>
2021-12-14 13:57:31 +0300

    feat(GuildMember): guild member avatar

--- slash/general/avatar.js
+++ slash/general/avatar.js
@@ -8,15 +8,29 @@ module.exports = {
             name: "user",
             description: "User to get avatar",
             type: 6,
+        },
+        {
+            name: "server_avatar",
+            description: "Get member avatar in this server",
+            type: 5,
         }
     ],
     category: "general",
     run: async(interaction) => {
-        const user = interaction.options.getUser('user') || interaction.user;
+        const member = interaction.options.getMember('user') || interaction.member;
+        const isMemberAvatar = interaction.options.getBoolean('server_avatar');
+        if (isMemberAvatar) {
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(member.user.tag, member.avatarURL({ dynamic: true }))
+            .setDescription(`[Avatar Link](${member.avatarURL({ dynamic: true, size: 4096 })})`)
+            .setImage(member.avatarURL({ dynamic: true, size: 4096 }))
+            .setFooter(`Requested By ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+            return interaction.reply({ embeds: [embed] })
+        }
         const embed = new Discord.MessageEmbed()
-        .setAuthor(user.tag, user.displayAvatarURL({ dynamic: true }))
-        .setDescription(`[Avatar Link](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`)
-        .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+        .setAuthor(member.user.tag, member.user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`[Avatar Link](${member.user.displayAvatarURL({ dynamic: true, size: 4096 })})`)
+        .setImage(member.user.displayAvatarURL({ dynamic: true, size: 4096 }))
         .setFooter(`Requested By ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
         interaction.reply({ embeds: [embed] })
     }
