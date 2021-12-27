07eecb64f5a107011dd7f626c42d5fb8b554abdb
Fnr <ah2014bh@outlook.com>
2021-12-28 01:05:21 +0300

    vanity url events

---
+++ events/guild/guildVanityURLAdd.js
@@ -0,0 +1,12 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports = async(guild, vanityURL) => {
+    const logChannel = client.channels.cache.get(config.log_channel_id);
+    if (!logChannel) return;
+    const embed = new Discord.MessageEmbed()
+    .setAuthor({ name: guild.name, iconURL: guild.iconURL() })
+    .setDescription(`**${guild.name} has added a vanity url** [URL](${vanityURL})`)
+    .setTimestamp()
+    return logChannel.send({ embeds: [embed] });
+}
