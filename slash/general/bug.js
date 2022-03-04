b8417a1351520dc2368711d4af700f58149ca115
Fnr <ah2014bh@outlook.com>
2022-03-04 19:28:48 +0300

    chore: format

--- slash/general/bug.js
+++ slash/general/bug.js
@@ -1,29 +1,24 @@
-const { ActionRow, TextInputComponent, TextInputStyle, Modal } = require('discord.js');
+const { ActionRow, TextInputComponent, TextInputStyle, Modal } = require("discord.js");
 
 module.exports = {
-    name: "bug",
-    description: "submit a bug",
-    run: async(interaction) => {
-        const modal = new Modal()
-        .setTitle('Bugs form')
-        .setCustomId('bug_form')
+	name: "bug",
+	description: "submit a bug",
+	run: async (interaction) => {
+		const modal = new Modal().setTitle("Bugs form").setCustomId("bug_form");
 
-        const bugTitle = new TextInputComponent()
-        .setCustomId('bug_title')
-        .setLabel('Bug Title')
-        .setStyle(TextInputStyle.Short)
+		const bugTitle = new TextInputComponent()
+			.setCustomId("bug_title")
+			.setLabel("Bug Title")
+			.setStyle(TextInputStyle.Short);
 
-        const bugDescription = new TextInputComponent()
-        .setCustomId('bug_description')
-        .setLabel('Bug description')
-        .setStyle(TextInputStyle.Paragraph)
+		const bugDescription = new TextInputComponent()
+			.setCustomId("bug_description")
+			.setLabel("Bug description")
+			.setStyle(TextInputStyle.Paragraph);
 
-        const rows = [bugTitle, bugDescription].map(
-            (component) => new ActionRow().addComponents(component)
-        )
+		const rows = [bugTitle, bugDescription].map((component) => new ActionRow().addComponents(component));
 
-        modal.addComponents(...rows);
-        await interaction.showModal(modal)
-        
-    }
-}
+		modal.addComponents(...rows);
+		await interaction.showModal(modal);
+	},
+};
